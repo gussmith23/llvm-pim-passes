13cdedfa8d0bfa3581d18fe0acb4b1a5003eb275
gussmith23 <hfs5022@psu.edu>
2018-02-20 13:04:29 -0500

    first commit

---
+++ pim-pass/pim-pass.cc
@@ -0,0 +1,20 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace {
+  class PimPass : public llvm::BasicBlockPass {
+    public:
+      static char ID;
+      PimPass() : llvm::BasicBlockPass(ID) {}
+
+      bool runOnBasicBlock(llvm::BasicBlock& basicBlock) {
+        llvm::errs() << "Hello\n";
+        return true;
+      }
+  };
+}
+
+char PimPass::ID = 0;
+static llvm::RegisterPass<PimPass> unused("pim-pass", "PIM Pass");
+// TODO I still don't understand the optional params to RegisterPass
