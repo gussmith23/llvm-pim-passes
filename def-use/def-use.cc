941392bd55fe39e19cd524e0571248706e41e957
gussmith23 <hfs5022@psu.edu>
2018-03-17 14:20:49 -0400

    Def-use pass finds simple load-load-op-store or load-immediate-op-store (and so on) combinations.

--- def-use/def-use.cc
+++ def-use/def-use.cc
@@ -4,8 +4,37 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/InstIterator.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Instructions.h"
 
 namespace {
+
+  /***
+   * Not a great name for this class.
+   * Takes a number of inputs, which can be loads or immediates;
+   * takes an operation;
+   * takes a store.
+   * Represents a chain which can be outsourced to PIM.
+   */
+  class LoadLoadOpStore {
+    public:
+      LoadLoadOpStore(std::vector<llvm::Value*> inputs, llvm::Value* op, std::vector<llvm::Value*> stores)
+        : inputs(inputs), op(op), stores(stores) {};
+
+      friend llvm::raw_ostream& operator<<(llvm::raw_ostream& ostream, const LoadLoadOpStore& llos) {
+        ostream << "Inputs:\n";
+        for (llvm::Value* input : llos.inputs) ostream << "\t" << *input << "\n";
+        ostream << "Operation:\n\t" << *llos.op << "\n";
+        ostream << "Outputs:\n";
+        for (llvm::Value* output : llos.stores) ostream << "\t" << *output << "\n";
+        return ostream;
+      }
+    
+    private:
+      std::vector<llvm::Value*> inputs;
+      llvm::Value* op;
+      std::vector<llvm::Value*> stores;
+  };
+
   class DefUse : public llvm::FunctionPass {
     public:
       static char ID;
@@ -17,7 +46,8 @@ namespace {
           worklist.push_back(&*I);
         }
 
-        // def-use chain for Instruction
+        std::vector<LoadLoadOpStore> foundLlos;
+
         for(std::vector<llvm::Instruction*>::iterator iter = worklist.begin(); iter != worklist.end(); ++iter){
           llvm::Instruction* instr = *iter;
 
@@ -29,38 +59,47 @@ namespace {
               || instr->getOpcode() == llvm::Instruction::Xor))
             continue;
 
-          llvm::errs() << "OPERANDS (live-ins):\n";
+          bool allLoadsOrImmediates = true;
           for (llvm::Use& use : instr->operands()) {
-            llvm::errs() << *use.get() << "\n"; 
+            if (llvm::Instruction* useInstr = llvm::dyn_cast<llvm::Instruction>(use.get())) {
+              if (useInstr->getOpcode() == llvm::Instruction::Load) {
+              } else {
+                llvm::errs() << "Found an operand that is an instruction other than Load: " << *use.get() << "\n";
+                allLoadsOrImmediates = false;
+              }
+            } else if (use->getType()->getTypeID() == llvm::Type::HalfTyID
+                        || use->getType()->getTypeID() == llvm::Type::FloatTyID
+                        || use->getType()->getTypeID() == llvm::Type::DoubleTyID
+                        || use->getType()->getTypeID() == llvm::Type::IntegerTyID) {
+              
+            } else {
+              llvm::errs() << "Found an operand that is of an unsupported type: " << use->getType() << "\n";
+              allLoadsOrImmediates = false;
+            }
           }
 
-          llvm::errs() << "INSTRUCTION: " <<*instr << "\n"; 
-          
-          llvm::errs() << "USERS (live-outs):\n";
+          bool allStores = true;
           for (llvm::User* user : instr->users()) {
-            llvm::errs() << *user << "\n"; 
+            if (llvm::dyn_cast<llvm::StoreInst>(user)) {
+            } else {
+              llvm::errs() << "Found a user which isn't a store: " << *user << "\n"; 
+              allStores = false;
+            }
           }
 
-          llvm::errs() << "\n";
-
-          /*
-          for(llvm::Value::use_iterator i = instr->use_begin(), ie = instr->use_end(); i!=ie; ++i){
-            llvm::Value *v = *i;
-            llvm::Instruction *vi = llvm::dyn_cast<llvm::Instruction>(*i);
-            llvm::errs() << "\t\t" << *vi << "\n"; 
-          }*/ 
+          if (allLoadsOrImmediates && allStores) {
+            foundLlos.push_back(
+                LoadLoadOpStore(
+                  std::vector<llvm::Value*>(instr->operands().begin(), instr->operands().end()),
+                  instr,
+                  std::vector<llvm::Value*>(instr->users().begin(), instr->users().end())) 
+                );
+          }
         } 
-       /* 
-        // use-def chain for Instruction 
-        for(std::vector<llvm::Instruction*>::iterator iter = worklist.begin(); iter != worklist.end(); ++iter){
-          llvm::Instruction* instr = *iter;
-          llvm::errs() << "use: " <<*instr << "\n"; 
-          for (llvm::User::op_iterator i = instr -> op_begin(), e = instr -> op_end(); i != e; ++i) {
-            llvm::Value *v = *i;
-            llvm::Instruction *vi = llvm::dyn_cast<llvm::Instruction>(*i);
-            llvm::errs() << "\t\t" << *vi << "\n"; 
-          } 
-        }*/
+
+        for (LoadLoadOpStore llos : foundLlos) {
+          llvm::errs() << llos << "\n";
+        }
 
         return false;
       }
